b2a99baa162c14396902efc21bbb57da152e4bac
Ryan McGeehan <billygoatmagoo@gmail.com>
2016-07-19 14:52:16 -0700

    Configuration is now down through .env

--- README.md
+++ README.md
@@ -110,3 +110,6 @@ function post {
 \# post "2.2.2.2" "ID-C"
 > BAD
 ```
+
+## Configuration
+Configuration is done within .env or the host's environment variables. See https://docs.docker.com/compose/env-file/
--- configuration.go
+++ configuration.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-      "fmt"
-      "encoding/json"
       "os"
       "flag"
+      "fmt"
     )
 
 
@@ -19,18 +18,29 @@ type Configuration struct {
 var c Configuration = readConfig()
 
 func readConfig() (c Configuration) {
-  flag.Parse()
+  fmt.Println("Reading config!")
   //Command Line Flags
-  var config_file string
-  flag.StringVar(&config_file, "conf", "./conf.json", "path to config file")
+  var flag_host string
+  flag.StringVar(&flag_host, "host", os.Getenv("AUTHTABLES_HOST"), "hostname for redis")
+  var flag_port string
+  flag.StringVar(&flag_port, "port", os.Getenv("AUTHTABLES_PORT"), "port for redis")
+  var flag_pw string
+  flag.StringVar(&flag_pw, "password", os.Getenv("AUTHTABLES_PW"), "password for redis")
+  flag.Parse()
 
-  //May need to come from CLI, built in for now
-  file, _ := os.Open(config_file)
-  decoder := json.NewDecoder(file)
+  fmt.Println("Reading config: " + flag_host + flag_port + flag_pw)
+  //We're going to load this with config data. See struct!
   configuration := Configuration{}
-  err := decoder.Decode(&configuration)
-  if err != nil {
-    fmt.Println("error:", err)
-  }
+
+  //command line flag is blank, use ENV instead
+
+  configuration.Host = os.Getenv("AUTHTABLES_HOST")
+  configuration.Port = os.Getenv("AUTHTABLES_PORT")
+  configuration.Password = os.Getenv("AUTHTABLES_PW")
+
+  //configuration.Host = flag_host
+  //configuration.Port = flag_port
+  //configuration.Password = flag_pw
+
   return configuration
 }
