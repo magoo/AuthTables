d4f50ee3438838e83efb5594b520c0a800027bf3
Ryan McGeehan <billygoatmagoo@gmail.com>
2016-07-19 21:29:09 -0700

    gofmt

--- build_test.go
+++ build_test.go
@@ -10,20 +10,20 @@ import "github.com/willf/bloom"
 
 func TestPrintLine(t *testing.T) {
 	// test stuff here...
-  fmt.Println("Print line works, so there's that.")
+	fmt.Println("Print line works, so there's that.")
 }
 
 func TestLoad(t *testing.T) {
-  writeRecord([]byte("asdf"))
+	writeRecord([]byte("asdf"))
 }
 
-func TestWWWServer(t *testing.T){
-  ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func TestWWWServer(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Testing the client")
 	}))
 	defer ts.Close()
 
-  res, err := http.Get(ts.URL)
+	res, err := http.Get(ts.URL)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -37,13 +37,13 @@ func TestWWWServer(t *testing.T){
 }
 
 func TestBloom(t *testing.T) {
-  var filter = bloom.New(1000000*n, 5) // load of 20, 5 keys
-  if filter.Test([]byte("shouldnotexist")){
-    log.Fatal("Bloom filter detected a string that wasn't in filter")
-  }
-  filter.Add([]byte("exists"))
-  if !filter.Test([]byte("exists")){
-    log.Fatal("Bloom filter could not detect a string that was in filter")
-  }
+	var filter = bloom.New(1000000*n, 5) // load of 20, 5 keys
+	if filter.Test([]byte("shouldnotexist")) {
+		log.Fatal("Bloom filter detected a string that wasn't in filter")
+	}
+	filter.Add([]byte("exists"))
+	if !filter.Test([]byte("exists")) {
+		log.Fatal("Bloom filter could not detect a string that was in filter")
+	}
 
 }
