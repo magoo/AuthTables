6a4aead529f0f59af72a89fe51fa46886b87936d
Ryan McGeehan <billygoatmagoo@gmail.com>
2016-07-19 21:11:07 -0700

    reduce code in main

--- Dockerfile
+++ Dockerfile
@@ -9,12 +9,13 @@ RUN go get github.com/willf/bloom \
            gopkg.in/redis.v4
 
 # Add our files
-ADD authtables.go authtables.go
+ADD main.go main.go
 ADD .env .env
 ADD configuration.go configuration.go
+ADD datastore.go datastore.go
 
 # Build app
-RUN go build authtables.go configuration.go
+RUN go build authtables.go configuration.go datastore.go
 
 # Default runs on 8080
 EXPOSE 8080
---
+++ datastore.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+      "gopkg.in/redis.v4"
+      "github.com/willf/bloom"
+    )
+
+//Main data structure for users. Every request we receive is a Record
+type Record struct {
+  UID string `json:"uid"`
+  IP string `json:"ip"`
+  MID string `json:"mid"`
+}
+
+//Main data structure for Bloom and Redis.
+type RecordHashes struct {
+  uid     []byte
+  uid_mid []byte
+  uid_ip  []byte
+  uid_all []byte
+  ip_mid  []byte
+  mid_ip  []byte
+}
+
+//Take us online to Redis
+var client = redis.NewClient(&redis.Options{
+    Addr:     c.Host + ":" + c.Port,
+    Password: c.Password, // no password set
+    DB:       0,  // use default DB
+})
+
+//Bloom Filter
+var n uint = 1000
+var filter = bloom.New(1000000*n, 5) // load of 20, 5 keys
